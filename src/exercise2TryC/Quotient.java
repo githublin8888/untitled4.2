f765b473d02dc6ee9c478b25b667cd4abcffaca6
githublin8888 <chunlinzhou2017@hotmail.com>
2023-02-01 11:14:16 +0100

    //10. Uppdatera programmet så att man enbart behöver läsa in det värde som inte är korrekt

--- src/exercise2TryC/Quotient.java
+++ src/exercise2TryC/Quotient.java
@@ -4,43 +4,51 @@ import java.util.Scanner;
 
 public class Quotient {
     public static void main(String[] args) {
-        boolean running=true;
-        while (running){
+        boolean running = true;
+        while (running) {
 
-        Scanner scan = new Scanner(System.in);
-        double nr1 = 0;
-        double nr2 = 0;
-        double quotient = 0;
+            Scanner scan = new Scanner(System.in);
+            double nr1 = 0;
+            double nr2 = 0;
+            double quotient = 0;
 
-        boolean incorrect = true;
-        while (incorrect)
-        {
-            try {
-                System.out.println("Enter number 1: ");
-                nr1 = Double.parseDouble(scan.nextLine());
+            boolean incorrect = true;
+            while (incorrect) {
 
-                System.out.println("Enter number 2: ");
-                nr2 = Double.parseDouble(scan.nextLine());
+                boolean incorrect1=true;
+                while (incorrect1){
+                    try {
+                        System.out.println("Enter number 1: ");
+                        nr1 = Double.parseDouble(scan.nextLine());
+                        incorrect1=false;
+                    } catch (NumberFormatException e) {
+                        System.out.println("It must be a number.");
+                    }
+                }
+
+                boolean incorrect2=true;
+                while (incorrect2){
+                    try {
+                        System.out.println("Enter number 2: ");
+                        nr2 = Double.parseDouble(scan.nextLine());
+                        incorrect2=false;
+                    } catch (NumberFormatException e) {
+                        System.out.println("It must be a number.");
+                    }
+                }
+
                 quotient = nr1 / nr2;
                 System.out.println(quotient);
-            }
-            catch (NumberFormatException e) {
-                System.out.println("It must be a number.");
+                running = false;
+                incorrect = false;
 
-            } catch (ArithmeticException e) {
-                System.out.println("The denominator can not be 0.");
-            } catch (Exception e) {
-                System.out.println("Something went wrong.");
+
             }
         }
-        }
-
-
-
-
-
-
-
-// boolean incorrect = true;
     }
 }
+/*catch (ArithmeticException e) {
+                    System.out.println("The denominator can not be 0.");
+                } catch (Exception e) {
+                    System.out.println("Something went wrong.");
+                }*/
